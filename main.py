5afd56851f23ff4a5dd014ec180d18babca01055
Joao Ricardo <feijaozinhofc@gmail.com>
2026-05-03 01:07:03 -0300

    cat-cliked-0.0.2

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from tkinter import *
 from PIL import Image, ImageTk
 import tkinter as tk
+import pygame
 
 window = tk.Tk()
+
+pygame.mixer.init()
+
+def button_music():
+    pygame.mixer.music.load("/home/joaodev/Projetos/cat_cliked/song/miau.mp3")
+    pygame.mixer.music.play()
+
+
 
 image = Image.open("/home/joaodev/Imagens/_ (4).jpeg")
 
 image_tk = ImageTk.PhotoImage(image)
 
-label = Label(window, image = image_tk)
-label.pack()
+cat = Button(window, image = image_tk,command=button_music)
+cat.pack()
 
-window = tk.Tk()
 
 
 
